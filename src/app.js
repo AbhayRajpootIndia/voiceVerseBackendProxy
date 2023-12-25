5f03d51aa2a54d1081cfce8ef13dd77266bf636a
AbhayRajpootIndia <abhay99043@gmail.com>
2023-12-25 16:15:35 +0530

    -> Added version and edited README.MD

--- src/app.js
+++ src/app.js
@@ -6,11 +6,13 @@ const postRouter = require('./routes/post');
 
 const app = express();
 
+const VERSION = '/v1';
+
 // Middleware
 app.use(express.json());
 
 // Routes
-app.use('/user', userRouter);
-app.use('/post', postRouter);
+app.use(`${VERSION}/user`, userRouter);
+app.use(`${VERSION}/post`, postRouter);
 
 module.exports = app;
